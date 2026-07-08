8b8d6c4f924ce353298c7fbdccd526b799b9313c
Zihad-1883 <zihad.pilot18@gmail.com>
2026-07-09 04:50:53 +0600

    query 5 complete

--- QUERY.sql
+++ QUERY.sql
@@ -205,3 +205,14 @@ FROM
   Bookings
   INNER JOIN Users ON Bookings.user_id = Users.user_id
   INNER JOIN Matches ON Bookings.match_id = Matches.match_id;
+
+
+-- Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed.
+
+SELECT
+  users.user_id,
+  full_name,
+  booking_id
+FROM
+  Users
+  LEFT JOIN Bookings ON Bookings.user_id = Users.user_id;
